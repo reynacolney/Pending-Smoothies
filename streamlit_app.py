baad9937b4a069286040945232d013cdba6b79da
reynacolney <70680885+reynacolney@users.noreply.github.com>
2025-02-26 20:21:11 -0700

    Updates to connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col,when_matched
 
 
@@ -8,8 +7,8 @@ from snowflake.snowpark.functions import col,when_matched
 st.title(":cup_with_straw: Pending Smoothie Orders:cup_with_straw:")
 st.write(
     """Orders that need to be filled""")
-
-session = get_active_session()
+cnx= st.connection ("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 
 if my_dataframe:
